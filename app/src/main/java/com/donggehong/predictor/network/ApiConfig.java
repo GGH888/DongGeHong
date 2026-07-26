7c3d86ad8ae53e7bbbd876fdfd5cad4e86fa87aa
GGH888 <975285574@qq.com>
2026-07-27 01:25:33 +0800

    Create ApiConfig.java

---
+++ app/src/main/java/com/donggehong/predictor/network/ApiConfig.java
@@ -0,0 +1,9 @@
+package com.donggehong.predictor.network;
+
+public class ApiConfig {
+    // OpenLigaDB（无需 Key）
+    public static final String OPENLIGA_BASE = "https://api.openligadb.de/";
+
+    // SportScore（无需 Key，作为备用）
+    public static final String SPORTSCORE_BASE = "https://sportscore.io/api/v1/";
+}
